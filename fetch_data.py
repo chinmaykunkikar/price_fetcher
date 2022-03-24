e5e6bc6723fd0cae60e94d347deabb009c4d0386
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-03-24 19:47:03 +0530

    fetch_data: overhaul fetch method One method for everything

--- fetch_data.py
+++ fetch_data.py
@@ -5,38 +5,41 @@ import from_fraazo
 import from_jiomart
 
 
-def fetch_products_from_jiomart(url_to_category, pincode_to_city, max_page_check):
+def fetch_products(retailer_url_category, **options):
     raw_json = []
-    for pin in pincode_to_city:
-        for url in url_to_category:
-            if "JIOMART" in url["retailer"]:
-                for page_number in range(max_page_check):
-                    raw_json.append(from_jiomart.fetch_data(
-                        url["url"]+"/page/"+str(page_number+1), pin["pincode"]))
-    return raw_json
-
-
-def fetch_products_from_fraazo(url_to_category):
-    raw_json = []
-    for url in url_to_category:
-        if "FRAAZO" in url["retailer"]:
-            raw_json.append(from_fraazo.fetch_data(url["url"]))
+    for url in retailer_url_category:
+        match url["retailer"]:
+            case "FRAAZO":
+                raw_json.append(from_fraazo.fetch_data(url["url"]))
+            case "JIOMART":
+                if "pincode" in options:
+                    for pin in options["pincode"]:
+                        for page_number in range(options["max_page_check"]):
+                            raw_json.append(from_jiomart.fetch_data(
+                                url["url"]+"/page/"+str(page_number+1), pin["pincode"]))
+            case _:
+                print("Retailer not found\n")
+                exit(1)
     return raw_json
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        print("Usage:\n\tpython " + sys.argv[0] + " (jiomart|fraazo)\n")
+        print("Usage:\n\tpython " + sys.argv[0] + " (fraazo|jiomart)\n")
         exit(1)
     else:
-        retailer = sys.argv[1]
-        if retailer == "jiomart":
-            raw_json = fetch_products_from_jiomart(
-                config.retailer_url_category, config.pincode_to_city, config.max_page_check)
-        elif retailer == "fraazo":
-            raw_json = fetch_products_from_fraazo(config.retailer_url_category)
-        else:
-            print("Usage:\n\tpython " + sys.argv[0] + " (jiomart|fraazo)\n")
-            exit(1)
+        retailer = sys.argv[1].lower()
+        match retailer:
+            case "fraazo":
+                raw_json = fetch_products(config.retailer_url_category)
+            case "jiomart":
+                raw_json = fetch_products(
+                    config.retailer_url_category,
+                    pincode=config.pincode_to_city,
+                    max_page_check=config.max_page_check)
+            case _:
+                print("Usage:\n\tpython " +
+                      sys.argv[0] + " (fraazo|jiomart)\n")
+                exit(1)
 
     config.write_to_raw_json_file(raw_json)
